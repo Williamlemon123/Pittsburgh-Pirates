8a7c1a8e8930a516ae5b9c8e94c61b7245373ced
Williamlemon123 <94806020+Williamlemon123@users.noreply.github.com>
2023-10-16 02:29:31 -0400

    Update Userss.py

--- OOP/OOP-Project-Resturant-main/Userss.py
+++ OOP/OOP-Project-Resturant-main/Userss.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
+
 class User(ABC):
-    def __init__(self, name, phone, email, address ) -> None:
+    def __init__(self, name, phone, email, address) -> None:
         self.name = name
         self.phone = phone
         self.email = email
         self.address = address
+
+class Order:  # Assuming a simple order class for the sake of demonstration
+    def __init__(self, items, bill):
+        self.items = items
+        self.bill = bill
 
 class Customer(User):
     def __init__(self, name, phone, email, address, money) -> None:
@@ -13,10 +19,12 @@ class Customer(User):
         self.due_amount = 0
         super().__init__(name, phone, email, address)
 
+    ##getting its value (getter)
     @property
     def order(self):
         return self.__order
-    
+
+    ##setting its value (setter)
     @order.setter
     def order(self, order):
         self.__order = order
@@ -26,56 +34,72 @@ class Customer(User):
         self.due_amount += order.bill
         print(f'{self.name} placed an order with bill {order.bill}')
 
-    def eat_Food(self , order):
-        print(f'{self.name} item food {order.items}')
+    def eat_food(self, order):
+        print(f'{self.name} ate food: {order.items}')
 
-    def pay_for_order(sekf, amount):
-        # TODO : submit the money to the manager
-        pass
+    def pay_for_order(self, order, manager):
+        if order.bill <= self.wallet:  # Check if the customer has enough money in the wallet
+            manager.collect_payment(order.bill)
+            self.due_amount -= order.bill
+            self.wallet -= order.bill
+            print(f"{self.name} paid {order.bill} for the order.")
+        else:
+            print(f"{self.name} does not have enough money to pay for the order.")
 
-    def give_tipe(self, tips_amount):
-        pass
+    def give_tip(self, tips_amount):
+        print(f"{self.name} gave a tip of {tips_amount}.")
+        self.wallet -= tips_amount
 
-    def write_review(Self, stars):
-        pass
+    def write_review(self, stars):
+        if 1 <= stars <= 5:
+            print(f"{self.name} gave a {stars}-star review.")
+        else:
+            print("Invalid number of stars. Please provide a value between 1 and 5.")
 
 
 class Employee(User):
-    def __init__(self, name, phone, email, address, salary, starting_date, depertment) -> None:
+    def __init__(self, name, phone, email, address, salary, starting_date, department) -> None:
         super().__init__(name, phone, email, address)
         self.salary = salary
         self.due = salary
         self.starting_date = starting_date
-        self.depertment = depertment
-    
+        self.department = department
+
     def receive_salary(self):
         self.due = 0
-
+        print(f"{self.name} received their salary.")
 
 
 class Chef(Employee):
-    def __init__(self, name, phone, email, address, salary, starting_date, depertment, cooking_item) -> None:
-        super().__init__(name, phone, email, address, salary, starting_date, depertment)
+    def __init__(self, name, phone, email, address, salary, starting_date, department, cooking_item) -> None:
+        super().__init__(name, phone, email, address, salary, starting_date, department)
         self.cooking_item = cooking_item
 
+
 class Server(Employee):
-    def __init__(self, name, phone, email, address, salary, starting_date, depertment) -> None:
+    def __init__(self, name, phone, email, address, salary, starting_date, department) -> None:
         self.tips_earning = 0
-        super().__init__(name, phone, email, address, salary, starting_date, depertment)
+        super().__init__(name, phone, email, address, salary, starting_date, department)
 
     def take_order(self, order):
-        pass
+        print(f"Server {self.name} has taken the order for {order.items}.")
 
     def transfer_order(self, order):
-        pass
+        print(f"Server {self.name} has transferred the order for {order.items} to the kitchen.")
 
     def serve_food(self, order):
-        pass
+        print(f"Server {self.name} has served the food for order {order.items}.")
 
     def receive_tips(self, amount):
         self.tips_earning += amount
+        print(f"Server {self.name} received a tip of {amount}.")
+
 
 class Manager(Employee):
-    def __init__(self, name, phone, email, address, salary, starting_date, depertment) -> None:
-        super().__init__(name, phone, email, address, salary, starting_date, depertment)
+    def __init__(self, name, phone, email, address, salary, starting_date, department) -> None:
+        super().__init__(name, phone, email, address, salary, starting_date, department)
+        self.balance = 0
 
+    def collect_payment(self, amount):
+        self.balance += amount
+        print(f"Manager {self.name} collected payment of {amount}. Current balance: {self.balance}")
